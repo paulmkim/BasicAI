07972237f4386a9bcfbd2f8ba59c9edc0ffe7cdd
Paul Kim <paul.m.kim321@gmail.com>
2014-12-23 17:41:57 -0800

    create array of words

--- main.cc
+++ main.cc
@@ -5,10 +5,40 @@ using namespace std;
 string keyword(string sentence)
 {
 	//ALLISON DOES THIS PART
+	string* words;
+	int numWords = 1;
+	for (int i = 0; i < sentence.size(); i++)
+	{
+		if (sentence[i] == ' ')
+		{
+			numWords++;
+		}
+	}
+
+	words = new string[numWords];
+	for (int i = 0; i < setence.size(); i++)
+	{
+		int counter = 0;
+		while (sentence[i] != ' ')
+		{
+			string[counter].append(sentence[i]);
+			i++;
+		}
+		counter++;
+		i++;
+	}
 }
 
 bool negative(string sentence)
 {
+	//wasn't, not, won't
+	for (int i = 0; i < sentence.size(); i++)
+	{
+		if (sentence[i] = ' ')
+		{
+			//blah
+		}
+	}
 	//PAUL DOES THIS PART JINGLE BELLS
 }
 int main()
