a2ab51972f19d45ba1828cd3610b656d68bfb15f
Paul Kim <paul.m.kim321@gmail.com>
2014-12-23 17:18:14 -0800

    Improved Skeleton

--- main.c
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	string response;
-	cout << "Hello! How are you!\n";
-}
---
+++ main.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string keyword(string sentence)
+{
+	//ALLISON
+}
+
+bool negative(string sentence)
+{
+	//PAUL
+}
+int main()
+{
+	string response;
+	string keyWord;
+	bool isNeg;
+	cout << "Hello! How are you!\n";
+	keyWord = keyword(response);
+	isNeg = negative(response);
+	if (keyWord == good && isNeg == TRUE)
+	{
+		//later
+	}
+
+}
